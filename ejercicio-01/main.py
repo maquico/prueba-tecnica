2c5ac3ba4605a2f520c54df949e07a0e5c905452
Angel Moreno <anmoreno@gbm.net>
2024-03-21 11:17:27 -0400

    cambios ejercicio 01

--- ejercicio-01/main.py
+++ ejercicio-01/main.py
@@ -1,4 +1,7 @@
 def detectar_palindromo(palabra):
     palabra_invertida = palabra[::-1]
     return True if palabra == palabra_invertida else False
-        
+
+if __name__ == "__main__":
+    palabra = input("Ingrese una palabra: ")
+    print("Es palindromo") if detectar_palindromo(palabra) else print("No es palindromo")
